dbd3c7e80f9bcc456743cfcb3438daf4e19a6f48
Lev <agsfer@gmail.com>
2026-01-15 21:31:57 +0200

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const devBase = '/'; // для localhost
+const prodBase = '/pacific-doc/'; // для GitHub Pages
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://agsfer.github.io',
-  	base: '/',
+  	base: process.env.NODE_ENV === 'production' ? prodBase : devBase,
 	integrations: [
 		starlight({
 			title: 'Pacific Ai Documentation',
